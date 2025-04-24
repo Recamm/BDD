ca636b16cecb407cbc389207bc6e16accdfc3af9
Camilo Reartes <71340165+Recamm@users.noreply.github.com>
2025-04-24 09:51:00 -0300

    Update classicmodels.sql

--- Triggers/classicmodels.sql
+++ Triggers/classicmodels.sql
@@ -1,4 +1,4 @@
-#1
+# 1
 
 create table customers_audit (
 	idAudit int auto_increment not null primary key,
@@ -11,38 +11,95 @@ create table customers_audit (
     country varchar(50)
 );
 
+# a
 delimiter //
 create trigger insertCustomerAudit after insert on customers
 for each row
 begin
 	insert into customers_audit values 
-    ("INSERT", current_date(), new.customerName, new.phone, new.addressLine1, new.city, new.country);
+    (null, "INSERT", current_date(), new.customerName, new.phone, new.addressLine1, new.city, new.country);
 end//
 delimiter ;
 
-# Autoincrement
-# Eliminar un trigger
 
-
-# 2
-
+# b
 delimiter //
 create trigger updateCustomerAudit before update on customers
 for each row
 begin
 	insert into customers_audit values
-    ("UPDATE", current_date(), old.customerName, old.phone, old.addressLine1, old.city, old.country);
+    (null, "UPDATE", current_date(), old.customerName, old.phone, old.addressLine1, old.city, old.country);
 end//
 delimiter ;
 
 
-# 3
-
+# c
 delimiter //
 create trigger deleteCustomerAudit before delete on customers
 for each row
 begin
 	insert into customers_audit values
-    ("DELETE", current_date(), old.customerName, old.phone, old.addressLine1, old.city, old.country);
+    (null, "DELETE", current_date(), old.customerName, old.phone, old.addressLine1, old.city, old.country);
 end//
+delimiter ;
+
+
+
+# 2
+create table employees_audit (
+	idAudit int auto_increment not null primary key,
+    operacion char(8),
+    Last_date_modified date,
+    employeeNumber int,
+    firstName varchar(50),
+    lastName varchar(50),
+    email varchar(50),
+    jobTitle varchar(50)
+);
+
+#a
+delimiter //
+create trigger insertEmlpoyeesAudit after insert on emlpoyees
+for each row
+begin
+	insert into emlpoyees_audit values
+    (null, "INSERT", current_date(), new.emlpoyeeNumber, new.firstName, new.lastName, new.email, new.jobTitle);
+end //
+delimiter ;
+
+#b
+delimiter //
+create trigger updateEmlpoyeesAudit before update on emlpoyees
+for each row
+begin
+	insert into emlpoyees_audit values
+    (null, "UPDATE", current_date(), old.emlpoyeeNumber, old.firstName, old.lastName, old.email, old.jobTitle);
+end //
+delimiter ;
+
+#b
+delimiter //
+create trigger deleteEmlpoyeesAudit before delete on emlpoyees
+for each row
+begin
+	insert into emlpoyees_audit values
+    (null, "delete", current_date(), old.emlpoyeeNumber, old.firstName, old.lastName, old.email, old.jobTitle);
+end //
+delimiter ;
+
+
+#3
+
+delimiter //
+create trigger deleteProductVerify before delete on products
+for each row
+begin
+	declare verify int;
+    select (datediff(current_date(), orderDate)/30.44) into verify from orders;
+    
+    if(verify > 2) then 
+		select "Error, tiene ordenes asociadas";
+    end if;
+    
+end //
 delimiter ;
