6223ae15e54ba3deb5ddb0ce8fdd5c340988347f
Recamm <devmmruiz@gmail.com>
2025-10-09 10:33:46 -0300

    Ejercicios introduccion terminados

--- mongodb/ejercicios.js
+++ mongodb/ejercicios.js
@@ -1,12 +1,14 @@
 // 1
 db.usuarios.insertMany([
     {
+        _id: 0,
         nombre: "Nicolas",
         apellido: "Orgnero",
         edad: 17,
         mail: "nicolasorgnero@gmail.com",
         compras: [
             {
+                _id: 0,
                 monto: 500,
                 productos: [
                     "manza",
@@ -16,12 +18,14 @@ db.usuarios.insertMany([
         ]
     },
     {
+        _id: 2,
         nombre: "Manuel",
         apellido: "Fernandez",
         edad: 18,
         mail: "manufer@gmail.com",
         compras: [
             {
+                _id: 1,
                 monto: 1000,
                 productos: [
                     "fideos",
@@ -35,4 +39,32 @@ db.usuarios.insertMany([
 // 2
 db.usuarios.find({edad: {$gt: 25}}, {nombre: 1, apellido: 1})
  
-// 3
+// 3 
+db.usuarios.find({$expr: {$eq: [{$size: "$compras"}, 2]}})
+
+// 4
+db.usuarios.find({}, {"compras.monto": 1}, { _id: 0 })
+
+// 5
+db.usuarios.find({$expr: {$gt: [{$size: "compras.productos"}, 5]}})
+
+// 6
+// db.usuarios.find({"compras._id": 1}, {compras: {$elemMatch: {_id: 1}}, _id: 0})
+
+// 7
+db.usuarios.updateOne({_id: 2}, {$push: {compras: {_id: 3, monto: 900, productos: ["harina", "manza"]}}})
+
+// 8
+db.usuarios.updateOne({nombre: "Lucas", apellido: "Martínez"}, {$set: {mail: "lucas.m@gmail.com"}})
+
+// 9
+db.usuarios.find({}, {"compras.id_compra": 1, "compras.monto": 1, _id: 1}).sort({"compras.monto": -1}).limit(3)
+
+// 10
+db.usuarios.find({compras: {$not: {$elemMatch: {productos: "televisor"}}}}, {nombre: 1, _id: 1})
+  
+// 11
+db.usuarios.find({"compras.productos": "laptop"})
+
+// 12
+db.usuarios.find({"compras": {$elemMatch: {"productos": "televisor", "monto": {$gt: 250}}}})
